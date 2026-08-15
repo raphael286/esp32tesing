a97758171830c12f517386bdb21941584025c70b
Julian Lavelle <lavellex86@outlook.com>
2026-08-14 18:43:10 -0700

    weird flashing problem

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,16 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
-#include "ft6336.hpp"
 #include "driver/i2c.h"
 
 #include "driver/gpio.h"
 #include "lvgl.h"
 #include "TFT_eSPI.h"
-#include "am/conn/physical.hpp"
-#include "am/management/touch.hpp"
+#include "am/conn/bluetooth.hpp"
 
-#define SDA -1
-#define SCL -1
+void tft_init();
 
-#define SCREEN_WIDTH = 320
-#define SCREEN_HEIGHT = 480
-
-static char name[] = "am_mc";
-static char url[] = "jewels86.me";
 TFT_eSPI tft = TFT_eSPI();
-esp_idf::ft6336<480, 320> ts;
-
-void tft_init(void);
-void ts_read(int touch_index, uint16_t* out_x, uint16_t* out_y);
-void ts_count(int* out_count);
-
 extern "C" void app_main(void) {
     tft_init();
 
@@ -41,19 +27,11 @@ extern "C" void app_main(void) {
     tft.setTextSize(2);
     tft.setCursor(0, 0);
     tft.println("\n\nTFT_eSPI initialized.");
-    am_i2c_init(SDA, SCL);
-    tft.println("I2C initialized.");
-
-    ts = esp_idf::ft6336<480, 320>(static_cast<i2c_port_t>(I2C_NUM_0));
-    if (!ts.initialize()) am_log("main", "ft6336_htcw failed to initialize");
-    tft.println("FT6336 series touch controller intialized.");
     
-    am_touch_controller_i tsi = { ts_read, ts_count };
-
-    vec2_u16_t touch;
-    am_touch_wait_0(&tsi, &touch);
-    tft.drawCircle(touch.x, touch.y, 10, TFT_RED);
-    tft.println("How you like them apples?");
+    am_ble_init();
+    tft.println("BLE initialized.");
+    am_ble_advertise("am_device", "", 9, 0);
+    tft.println("BLE advertising started.");
 }
 
 void tft_init() {
@@ -61,9 +39,3 @@ void tft_init() {
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK); 
 }
-
-void ts_read(int touch_index, uint16_t* out_x, uint16_t* out_y) {
-    ts.update();
-    ts.xy(out_x, out_y);
-}
-void ts_count(int* out_count) { ts.touches(); }
